5f1ab7921df2d6dfefacb3dc59a6373589a5f66a
Tony Mugavero <tmugavero@moovatom.com>
2013-06-12 11:14:28 -0600

    Cleaned up spacing, added comments, and fixed api error message.

--- everybit/utils.py
+++ everybit/utils.py
@@ -1,23 +1,27 @@
-import urllib
 import urllib2
 import json
 
 from configuration import *
 
 
-## Exceptions
 class EverybitError(Exception):
-  def __init__(self, message=None, http_body=None, http_status=None, json_body=None):
-    super(EverybitError, self).__init__(message)
-    self.http_body = http_body and http_body.decode('utf-8')
-    self.http_status = http_status
-    self.json_body = json_body
+    """
+    Custom Everybit Exception.
+    """
+
+    def __init__(self, message=None, http_body=None, http_status=None,
+                 json_body=None):
+
+        super(EverybitError, self).__init__(message)
+        self.http_body = http_body and http_body.decode('utf-8')
+        self.http_status = http_status
+        self.json_body = json_body
 
 class APIError(EverybitError):
-  pass
+    pass
 
 class APIConnectionError(EverybitError):
-  pass
+    pass
 
 
 class EverybitRequest(urllib2.Request):
@@ -25,9 +29,12 @@ class EverybitRequest(urllib2.Request):
     Override the urllib2 Request so we can easily set the method.
     """
 
-    def __init__(self, url, data=None, headers={},
-                 origin_req_host=None, unverifiable=False, method=None):
-       urllib2.Request.__init__(self, url, data, headers, origin_req_host, unverifiable)
+    def __init__(self, url, data=None, headers={}, origin_req_host=None,
+                 unverifiable=False, method=None):
+
+       urllib2.Request.__init__(
+           self, url, data, headers, origin_req_host, unverifiable
+       )
        self.method = method
 
     def get_method(self):
@@ -43,7 +50,6 @@ def make_request(url, method='GET', data={}):
     """
 
     try:
-        print api_key
         opener = urllib2.build_opener(urllib2.HTTPHandler)
         req = EverybitRequest(
             url,
@@ -56,6 +62,7 @@ def make_request(url, method='GET', data={}):
         )
         return opener.open(req).read()
     except Exception, e:
-        raise APIConnectionError(
-            message="It appears we were unable to communicate with the Everybit API.  Is your API key set?  Are you passing in correctly formatted data? %s" % e
-        )
+        message="%s We were unable to communicate with the Everybit API. \
+         Is your API key set?  \
+         Are you passing in correctly formatted data?" % e
+        raise APIConnectionError(message=message)
